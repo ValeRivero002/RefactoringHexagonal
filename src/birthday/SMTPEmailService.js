22793ec89fac7dedfa72e95ce2526eeed19962d6
Valeria Rivero <valeria.rivero@ucb.edu.bo>
2024-06-03 22:34:58 -0400

    Crear el adaptador SMTPEmailService llevar funciones sendBirthdatgreeting

---
+++ src/birthday/SMTPEmailService.js
@@ -0,0 +1,23 @@
+// adapters/SMTPEmailService.js
+import { EmailService } from "../ports/EmailService.js";
+
+export class SMTPEmailService extends EmailService {
+  constructor(smtpUrl, smtpPort, transport) {
+    super();
+    this.smtpUrl = smtpUrl;
+    this.smtpPort = smtpPort;
+    this.transport = transport;
+  }
+
+  sendBirthdayGreeting(employee) {
+    const message = {
+      host: this.smtpUrl,
+      port: this.smtpPort,
+      from: "sender@here.com",
+      to: [employee.getEmail()],
+      subject: "Happy Birthday!",
+      text: `Happy Birthday, dear ${employee.getFirstName()}!`,
+    };
+    this.transport.sendMail(message);
+  }
+}
