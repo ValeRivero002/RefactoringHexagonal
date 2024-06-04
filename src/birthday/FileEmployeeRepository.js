65aebc978f3b07dab1b3ce9079ba3826d336a10c
Valeria Rivero <valeria.rivero@ucb.edu.bo>
2024-06-03 22:33:00 -0400

    Crear el adaptador FileEmployeeRepository llevar funciones findemployeeswithbirthday y createemployeefromline

---
+++ src/birthday/FileEmployeeRepository.js
@@ -0,0 +1,30 @@
+// adapters/FileEmployeeRepository.js
+import fs from "fs";
+import path from "path";
+import { Employee } from "../domain/Employee.js";
+import { EmployeeRepository } from "../ports/EmployeeRepository.js";
+
+export class FileEmployeeRepository extends EmployeeRepository {
+  constructor(fileName) {
+    super();
+    this.fileName = fileName;
+  }
+
+  findEmployeesWithBirthday(date) {
+    const data = fs.readFileSync(path.resolve(this.fileName), "UTF-8");
+    const lines = data.split(/\r?\n/).slice(1);
+    return lines
+      .map((line) => this.createEmployeeFromLine(line))
+      .filter((employee) => employee.isBirthday(date));
+  }
+
+  createEmployeeFromLine(line) {
+    const employeeData = line.split(", ");
+    return new Employee(
+      employeeData[1],
+      employeeData[0],
+      employeeData[2],
+      employeeData[3]
+    );
+  }
+}
